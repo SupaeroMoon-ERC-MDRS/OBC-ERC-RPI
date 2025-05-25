e6736a5f06e17d366ee457920ea30f5ca9cd20e6
eseabro <62852911+eseabro@users.noreply.github.com>
2025-05-25 19:27:39 +0200

    still zeroing

--- src/erc_build/erc_build/steering_node.py
+++ src/erc_build/erc_build/steering_node.py
@@ -24,10 +24,10 @@ class SixWheelFourWSController(Node):
         cmd = Float64MultiArray()
 
         # Default: no turning → all angles = 0
-        angle_fl = 45.0 #2
-        angle_fr = 259.0 #1
+        angle_fl = 44.0 #2
+        angle_fr = 260.0 #1
         angle_rl = 248.0 #3
-        angle_rr = 41.0 #0 +ve CCW
+        angle_rr = 40.0 #0 +ve CCW
         vel_fl = vel_fr = vel_ml = vel_mr = vel_rl = vel_rr = 0.0
 
         if abs(angular_vel) > 1e-5 and abs(linear_vel) > 1e-5:
