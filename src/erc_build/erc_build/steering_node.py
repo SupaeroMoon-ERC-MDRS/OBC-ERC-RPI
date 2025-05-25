7b0ac93ddd4f3b82b3ffd638d63e87e5f45f856f
eseabro <62852911+eseabro@users.noreply.github.com>
2025-05-25 17:05:42 +0200

    fixing #0

--- src/erc_build/erc_build/steering_node.py
+++ src/erc_build/erc_build/steering_node.py
@@ -27,7 +27,7 @@ class SixWheelFourWSController(Node):
         angle_fl = 45.0 #2
         angle_fr = 257.0 #1
         angle_rl = 248.0 #3
-        angle_rr = 45.0 #0
+        angle_rr = 46.0 #0
         vel_fl = vel_fr = vel_ml = vel_mr = vel_rl = vel_rr = 0.0
 
         if abs(angular_vel) > 1e-5 and abs(linear_vel) > 1e-5:
