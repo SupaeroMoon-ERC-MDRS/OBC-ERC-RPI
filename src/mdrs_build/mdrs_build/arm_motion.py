fdb43a3ef7d0c576cf8ad0ad01fd98c868b139d2
eseabro <62852911+eseabro@users.noreply.github.com>
2026-01-18 17:37:35 +0100

    added elbow logic

--- src/mdrs_build/mdrs_build/arm_motion.py
+++ src/mdrs_build/mdrs_build/arm_motion.py
@@ -63,7 +63,10 @@ class IKServoController(Node):
             else:
                 self.current_x = x
                 self.get_logger().info(f"New X position: {self.current_x}")
-                self.compute_ik()
+                if self.current_x >= 0:
+                    self.compute_ik_up()
+                else:
+                    self.compute_ik_down()
 
         if abs(msg.linear.y) > 1e-4:
             dz = msg.linear.y
@@ -78,7 +81,10 @@ class IKServoController(Node):
             else:
                 self.current_y =y
                 self.get_logger().info(f"New Y position: {self.current_y}")
-                self.compute_ik()
+                if self.current_x >= 0:
+                    self.compute_ik_up()
+                else:
+                    self.compute_ik_down()
 
         if abs(msg.angular.x) > 1e-4:
             dtheta = msg.angular.x * 10
@@ -93,7 +99,7 @@ class IKServoController(Node):
             self.get_logger().info("No arm movement command received.")
 
 
-    def compute_ik(self):
+    def compute_ik_up(self):
         self.get_logger().info(f"Computing IK for current_x: {self.current_x}, current_y: {self.current_y}")
         y = self.current_y - self.l1  # Adjust for base height
         x = self.current_x
@@ -118,6 +124,33 @@ class IKServoController(Node):
         self.send_to_servo(self.theta_2_curr, self.theta_2)
 
         return q1, q2
+
+    def compute_ik_down(self):
+        self.get_logger().info(f"Computing IK for current_x: {self.current_x}, current_y: {self.current_y}")
+        y = self.current_y - self.l1  # Adjust for base height
+        x = self.current_x
+        d = math.sqrt(x**2 + y**2)
+        
+        q2 = math.acos(self.l2**2 + self.l3**2 - d**2) / (2 * self.l2 * self.l3)
+        # q1 = math.atan2(y, x) - math.acos(self.l2**2 + d**2 - self.l3**2) / (2 * self.l2 * d)
+        q1 = math.atan2(y, x) - math.atan2(self.l3 * math.sin(q2), self.l2 + self.l3 * math.cos(q2))
+        # Gemini's version of IK:
+        # cos_q2 = (self.l2**2 + self.l3**2 - d**2) / (2 * self.l2 * self.l3)
+        # cos_q2 = max(-1.0, min(1.0, cos_q2)) # Safety Clamp
+        # q2 = math.acos(cos_q2)
+
+        # cos_q1 = (self.l2**2 + d**2 - self.l3**2) / (2 * self.l2 * d)
+        # cos_q1 = max(-1.0, min(1.0, cos_q1)) # Safety Clamp
+        # q1 = math.atan2(y, x) - math.acos(cos_q1)
+        q1 = math.degrees(q1)
+        q2 = math.degrees(q2)
+        self.theta_1_curr = q1
+        self.theta_2_curr = q2
+        self.send_to_servo(self.theta_1_curr, self.theta_1)
+        self.send_to_servo(self.theta_2_curr, self.theta_2)
+
+        return q1, q2
+    
 
     def send_to_servo(self, q1, servo1):
         # a1 = q1 * 180/ 300
