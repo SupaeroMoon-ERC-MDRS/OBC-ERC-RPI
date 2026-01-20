8af4b64a75403e42bc861256835b6db5da8026b9
eseabro <62852911+eseabro@users.noreply.github.com>
2026-01-20 16:57:27 +0100

    fixing inverse kin

--- src/mdrs_build/mdrs_build/arm_motion.py
+++ src/mdrs_build/mdrs_build/arm_motion.py
@@ -127,10 +127,10 @@ class IKServoController(Node):
 
     def compute_ik(self, dir='up'):
         self.get_logger().info(f"Computing IK for current_x: {self.current_x}, current_y: {self.current_y}")
-        y = self.current_y  # Adjust for base height
-        x = self.current_x
-        d = math.sqrt(x**2 + y**2)
-        try: 
+        y = self.current_y  # Adjust for base height # 0.30046
+        x = self.current_x # 0
+        d = math.sqrt(x**2 + y**2)  # 0.30046
+        try:
             q2 = math.acos((self.l2**2 + self.l3**2 - d**2) / (2 * self.l2 * self.l3))
             if dir == 'up':
                 q2 = -q2
@@ -138,9 +138,11 @@ class IKServoController(Node):
             q1 = math.atan2(y, x) - math.atan2(self.l3 * math.sin(q2), self.l2 + self.l3 * math.cos(q2))
             q1 = math.degrees(q1)
             q2 = math.degrees(q2)
-            self.theta_1_curr = 270-q1
+
+            self.theta_1_curr = q1 # updates current
             self.theta_2_curr = q2
-            theta_1_servo = 135 + (90 - q1) # Example mapping, adjust sign as needed
+
+            theta_1_servo = 135 - (90 + q1) # Example mapping, adjust sign as needed
             theta_2_servo = 135 + q2
 
             self.get_logger().info(f"Computed angles: theta_1={theta_1_servo:.1f}, theta_2={theta_2_servo:.1f}")
