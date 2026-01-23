034d4023c9c4cacee68ea88f3878cede25a91e25
eseabro <62852911+eseabro@users.noreply.github.com>
2026-01-23 14:57:07 +0100

    added camera testing

---
+++ src/mdrs_build/launch/camera_testing.launch.py
@@ -0,0 +1,62 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+    pkg_my_robot = get_package_share_directory('mdrs_build')
+
+    camera_calib = os.path.join(pkg_my_robot, 'config', 'camera_calib.yaml')
+    return LaunchDescription([
+        # steering controller
+        # Node(
+        #     package='mdrs_build',
+        #     executable='diff_controller',
+        #     name='diff_controller',
+        #     output='screen'
+        # ),
+
+        # # Roboclaw motor driver
+        # Node(
+        #     package='mdrs_build',
+        #     executable='roboclaw_node',
+        #     name='roboclaw_node',
+        #     output='screen'
+        # ),
+
+
+        # This function drives the arm
+        # Node(
+        #     package='mdrs_build',
+        #     executable='arm_motion',
+        #     name='arm_motion',
+        #     output='screen'
+        # ),
+        # # Communication between remote control and rover
+        # Node(
+        #     package='mdrs_build',
+        #     executable='remote_messages_node',
+        #     name='remote_messages_node',
+        #     output='screen'
+        # ),
+        Node(
+            package='mdrs_build',
+            executable='camera_ros',
+            name='camera_node',
+            output='screen'
+        ),
+
+        Node(
+            package='mdrs_build',
+            executable='aruco_pub',
+            parameters=[{'target_marker_id': 0, 'camera_calib_file': camera_calib}] 
+        ),
+
+        Node(
+            package='mdrs_build',
+            executable='marker_follower',
+            name='marker_follower',
+            output='screen'
+        )
+
+    ])
