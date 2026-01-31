f783f2f82975beeea12d1c3c5c9f6fbd0ddfdb99
eseabro <62852911+eseabro@users.noreply.github.com>
2026-01-31 20:19:15 +0100

    re-increased angular

--- src/mdrs_build/mdrs_build/marker_follower.py
+++ src/mdrs_build/mdrs_build/marker_follower.py
@@ -19,7 +19,7 @@ class SimpleMarkerFollower(Node):
         self.max_angular_vel = 10.0
         
         # Gains for the proportional controller
-        self.kp_dist = 1.5
+        self.kp_dist = 0.5
         self.kp_ang = 1.5
         # Topics
         self.sub = self.create_subscription(
