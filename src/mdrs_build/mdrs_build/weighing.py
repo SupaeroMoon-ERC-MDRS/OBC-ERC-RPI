a61e8fce5b6ef17049a4434d7ba1640638887956
Eden <eden.buch-kornreich@student.isae-supaero.fr>
2026-02-01 13:23:24 +0100

    scale things

--- src/mdrs_build/mdrs_build/weighing.py
+++ src/mdrs_build/mdrs_build/weighing.py
@@ -5,7 +5,8 @@ from hx711 import HX711
 # Setup HX711
 hx = HX711(16, 20)
 hx.set_reading_format("MSB", "MSB")
-hx.set_reference_unit(406.84)  # Use your calculated reference unit here
+ref = float(input("Please give reference unit : "))
+hx.set_reference_unit(ref)  # Use your calculated reference unit here
 hx.reset()
 hx.tare()
 
@@ -14,7 +15,7 @@ print("Press Ctrl+C to exit")
 
 try:
     while True:
-        weight = hx.get_weight(3)  # Average of 3 readings
+        weight = hx.get_weight(10)  # Average of 3 readings
         print(f"Weight: {weight:.1f}g")
         
         hx.power_down()
